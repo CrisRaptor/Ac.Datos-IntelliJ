658a908960e937567f3c18158af7337e341ba3bb
CrisR <cristiangarcha7@gmail.com>
2025-02-14 13:53:29 +0100

    ClienteThymeleaf

---
+++ ClienteThymeleafEmpleados/src/main/java/com/iesbelen/dam/apirest/clientethymeleafempleados/ClienteThymeleafEmpleadosApplication.java
@@ -0,0 +1,13 @@
+package com.iesbelen.dam.apirest.clientethymeleafempleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteThymeleafEmpleadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteThymeleafEmpleadosApplication.class, args);
+    }
+
+}
---
+++ ClienteThymeleafEmpleados/src/test/java/com/iesbelen/dam/apirest/clientethymeleafempleados/ClienteThymeleafEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesbelen.dam.apirest.clientethymeleafempleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteThymeleafEmpleadosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

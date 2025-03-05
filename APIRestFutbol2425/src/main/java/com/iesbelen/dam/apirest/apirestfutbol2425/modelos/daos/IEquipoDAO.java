3d790809ea338131e026e84b2a6f061f8ecd6cf3
CrisR <cristiangarcha7@gmail.com>
2025-03-05 14:31:44 +0100

    Reestructuracion

--- APIRestFutbol2425/src/main/java/com/iesbelen/dam/apirest/apirestfutbol2425/modelos/daos/IEquipoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.iesbelen.dam.apirest.apirestfutbol2425.modelos.daos;
-
-import com.iesbelen.dam.apirest.apirestfutbol2425.modelos.entidades.Equipo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IEquipoDAO extends CrudRepository<Equipo, Integer> {
-
-    public Equipo findByNombre(String nombre);
-}
--- APIRestFutbol2425Thymeleaf/src/main/java/com/iesbelen/dam/apirest/apirestfutbol2425thymeleaf/modelo/daos/IEquipoDAO.java
+++
@@ -1,7 +0,0 @@
-package com.iesbelen.dam.apirest.apirestfutbol2425thymeleaf.modelo.daos;
-
-import com.iesbelen.dam.apirest.apirestfutbol2425thymeleaf.modelo.entidades.Equipo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IEquipoDAO extends CrudRepository<Equipo, Integer> {
-}
